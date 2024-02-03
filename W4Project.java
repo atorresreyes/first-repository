1bbf5c50d5aff8c0cebc190f02b9c0fcf94cb318
Ayari Torres-Reyes <agtorresreyes@gmail.com>
2024-02-03 16:07:31 -0600

    adding Week 4 Project

--- W4Project.java
+++ W4Project.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class W4Project {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	//	Set<String> newList = new HashSet<String>();
 		/*
 		 * 
 		 * 
@@ -21,7 +19,8 @@ public class W4Project {
 				 * code). Print the result to the console.
 				 */
 		  		System.out.print("1a: ");
-		  		System.out.println(ages[ages.length - 1] - ages[0]);
+		  		System.out.println(ages[ages.length - 1] - ages[0]); 
+		  			//array.length - 1 will always be the last index in the array since the array is 0 based
 		  
 				 /* b. Create a new array of int called ages2 with 9 elements (ages2 will be
 				 * longer than the ages array, and have more elements).
@@ -38,6 +37,7 @@ public class W4Project {
 		  		int[] ages2 = { 3 , 5 , 7 , 9 , 10 , 30 , 50 , 60 , 100};
 		  		System.out.print("1b: ");
 		  		System.out.println(ages2[ages2.length - 1] - ages2[0]);
+		  			//array.length - 1 will always be the last index in the array since the array is 0 based
 		  		
 				 /* 
 				 * c. Use a loop to iterate through the array and calculate the average age.
@@ -48,6 +48,9 @@ public class W4Project {
 		  			sum += age;
 		  		}
 		  		int averageAge = sum / ages.length;
+		  			//here we need the length of the array to find the number of elements, 
+		  			//not the index of where the last element is , so we do not -1 from length
+		  		
 		  		System.out.print("1c: ");
 		  		System.out.println(averageAge);
 		/* 
@@ -61,27 +64,28 @@ public class W4Project {
 				 */
 		  		int sumNames = 0;
 		  		for (String name : names) {
-		  			sumNames += name.length();
+		  			sumNames += name.length(); //for each name, we add the length of the name to sumNames
 		  		}
-		  		int averageLength = sumNames / names.length;
+		  		int averageLength = sumNames / names.length; //divide by the number of names to find average
 		  		System.out.print("2a. ");
 		  		System.out.println(averageLength);
+		  		
 				 /* b. Use a loop to iterate through the array again and concatenate all the
 				 * names together, separated by spaces, and print the result to the console.
 				 */
-		  		StringBuilder allNames = new StringBuilder();
+		  		StringBuilder allNames = new StringBuilder(); //using StringBuilder to practice using StringBuilder
 		  		for (String name : names) {
 		  			allNames.append(name + " ");
 		  		}
 		  		System.out.print("2b. ");
-		  		System.out.println(allNames);
+		  		System.out.println(allNames.toString()); //convert StringBuilder to String
 		  
 		 // 3. How do you access the last element of any array?
-		  		System.out.println("3.  names[names.length - 1]");
+		  		System.out.println("3.  names[names.length - 1]"); //array.length - 1 will always be the last index in the array since the array is 0 based
 		  		System.out.println("    Output: " +  names[names.length - 1]);
 		 
 		 // 4. How do you access the first element of any array?
-		  		System.out.println("4.  names[0]");
+		  		System.out.println("4.  names[0]"); //array is 0 based so the first element will always be at 0
 		  		System.out.println("    Output: " +  names[0]);
 		 
 		 /* 5. Create a new array of int called nameLengths. Write a loop to iterate over
@@ -90,31 +94,32 @@ public class W4Project {
 		 */
 		  	int[] nameLengths = new int[names.length];
 		  	for (int i = 0; i < names.length; i++) {
-		  		nameLengths[i] = names[i].length();
+		  		nameLengths[i] = names[i].length(); //puts all the lengths into the array
 		  	}
-		  	StringBuilder nameLengthsPrint = new StringBuilder();
+		  	StringBuilder nameLengthsPrint = new StringBuilder(); //using StringBuilder to practice using StringBuilder
 		  	for (int length : nameLengths) {
-		  		nameLengthsPrint.append(length + " ");
+		  		nameLengthsPrint.append(length + " "); //puts the array elements into the String so we can print them
 		  	}
 		  	System.out.print("5.  nameLengths = ");
-		  	System.out.println(nameLengthsPrint);
+		  	System.out.println(nameLengthsPrint.toString()); //convert StringBuilder to String
 		  	
 		 /* 6. Write a loop to iterate over the nameLengths array and calculate the sum
 		 * of all the elements in the array. Print the result to the console.
 		 */
 		  	int sumLengths = 0;
 		  	for (int nameLength : nameLengths) {
-		  		sumLengths += nameLength;
+		  		sumLengths += nameLength; // for each element, we add it to sumLengths to find the total sum
 		  	}
 		 	System.out.print("6.  nameLengths sum = ");
 		  	System.out.println(sumLengths);
+		  	
 		 /* 7. Write a method that takes a String, word, and an int, n, as arguments and
 		 * returns the word concatenated to itself n number of times. (i.e. if I pass in
 		 * “Hello” and 3, I expect the method to return “HelloHelloHello”).
 		 */ 
 		  	
 		  	System.out.print("7.  wordTimesNum = ");
-		  	System.out.println(wordTimesNum("Hello", 3));
+		  	System.out.println(wordTimesNum("Hello", 3)); //calling on the method
 		  
 		  	
 		 /* 8. Write a method that takes two Strings, firstName and lastName, and returns
@@ -131,34 +136,40 @@ public class W4Project {
 		 * all the ints in the array is greater than 100.
 		 */
 		  	System.out.print("9.  Sum Greater than 100?: ");
-		  	System.out.println(isGreaterThan100(ages));
+		  	System.out.println(isGreaterThan100(ages)); 
 		  	
 		 /* 10. Write a method that takes an array of double and returns the average of
 		 * all the elements in the array.
 		 */
 		  	double[] arr = {1.2 , 2.3 , 4.5 , 6.7 , 8.9 , 0.01};
 		  	System.out.print("10. Average of Double: ");
-		  	System.out.printf("%.3f %n" , findsAverage(arr));
+		  	System.out.printf("%.3f %n" , findsAverage(arr)); //printing out the average up to 3 points and 
 		  	
 		 /* 
 		 * 11. Write a method that takes two arrays of double and returns true if the
 		 * average of the elements in the first array is greater than the average of the
 		 * elements in the second array.
 		 */ 
-		  	double[] arr2 = {0.9 , 8.7, 6.5, 4.3 , 2.1};
+		  	double[] arr2 = {0.9 , 8.7, 6.5, 4.3 , 2.1}; 
 		  	System.out.print("11. First is Greater: ");
-		  	System.out.println(oneIsGreater(arr, arr2));
+		  	System.out.println(oneIsGreater(arr, arr2)); 
 		  	
 		  	
 		 /* 12. Write a method called willBuyDrink that takes a boolean isHotOutside, and
 		 * a double moneyInPocket, and returns true if it is hot outside and if
 		 * moneyInPocket is greater than 10.50.
 		 */
-		  	Boolean isHotOutside = true;
+		  	Boolean isHotOutside = true; 
 		  	double moneyInPocket = 15.75;
 		  	System.out.print("12. I will buy a nice drink today: ");
-		  	System.out.println(willBuyDrink(isHotOutside, moneyInPocket));
+		  	System.out.println(willBuyDrink(isHotOutside, moneyInPocket)); 
 		  		
+		  	
+		  	
+		  	
+		  	System.out.println(); // This Line Has Been Left Intentionally Blank (for spacing) 
+		 
+		  	
 		  	
 		 /* 
 		 * 13. Create a method of your own that solves a problem. In comments, write
@@ -166,11 +177,10 @@ public class W4Project {
 		 * 
 		 */
 		  	//Creating a method that makes an ice cream cone
+		  	//Practice using a do while loop, scanner, and looping until getting an acceptable answer from user
 		  	//To create a visual for the end of the create ice cream program
-		  	System.out.println(); // This Line Has Been Left Intentionally Blank (for spacing) 
 		  	
 		  	System.out.println("13. Ice Cream time! ");
-		 // 	Scanner sc = new Scanner(System.in);
 		  	System.out.println("Hello! You can have up to 3 scoops of ice cream.");
 		  	
 		  	//Entering ice cream making process
@@ -188,7 +198,7 @@ public class W4Project {
 	  	if (scoops > 0 && scoops < 4) { //checking to see if number is either 1 , 2 , or 3
 	  		System.out.println("Enjoy!"); 
 	  		buildIceCream(scoops); //if 1 , 2 , or 3, enter into building Ice Cream method
-	  	} else if (scoops > 0 && scoops >= 4) { //if not 1-3, remind user it is up to 3
+	  	} else if (scoops > 0 && scoops >= 4) { //if entered 4 or above, remind user it is up to 3
 	  		System.out.print("That's too much ice cream! You can have up to ");
 	  		System.err.print("3");
 	  		System.out.print(" scoops.");
@@ -212,68 +222,38 @@ public class W4Project {
 		  	System.out.println("  - - - -  ");
 			System.out.println("   - - -   ");
 			i++;
-  		} while (i < scoops);
+  		} while (i < scoops); //do while loop will always go at least Once , so the scoop will be at least once, until it reaches the number of scoops needed
   		//Cone
 	  	System.out.println("   \\###/   ");
 	  	System.out.println("    \\#/    ");
 	  	System.out.println("     V     "); 
   	}
-
-
-	//Method for 7 
-	public static String wordTimesNum(String word, int n) {
-		StringBuilder newWord = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			newWord.append(word);
-		}
-		
-		return newWord.toString();
-	}
-	
-	//Method for 8
-	public static String makeFullName(String firstName, String lastName) {
-		String fullName = firstName + " " + lastName;
-		return fullName;
-	}
-	
-	//Method for 9
-
-	public static Boolean isGreaterThan100(int[] ages) {
-		Boolean isGreater;
-		int sum = 0;
-		for (int age : ages) {
-			sum += age;
-		}
-		if (sum > 100) {
-			isGreater = true;
+  	
+	//Method for 12
+	public static Boolean willBuyDrink(Boolean isHotOutside, double moneyInPocket) {
+		Boolean buyDrink;
+		if ((isHotOutside == true) && (moneyInPocket > 10.50)) { //testing that BOTH are true
+			buyDrink = true;
 		} else {
-			isGreater = false;
+			buyDrink = false;
 		}
-		return isGreater;
+		return buyDrink;
 	}
-	
-	//Method for 10
-	public static double findsAverage(double[] arr) {
-		double sum = 0;
-		for (double num : arr) {
-			sum += num;
-		}
-		double average = sum / arr.length;
-		return average;
-	}
-	
+
 	//Method for 11
 	public static Boolean oneIsGreater(double[] arr, double[] arr2) {
 		Boolean isGreater;
 		double sum1 = 0;
 		double sum2 = 0;
 		
+		//finding the total sum of each array
 		for (double num : arr) {
 			sum1 += num;
 		}
 		for (double num : arr2) {
 			sum2 += num;
 		}
+		//testing to see if sum1 is bigger than sum2 and returning true or false
 		if (sum1 > sum2) {
 			return isGreater = true;
 		} else {
@@ -281,15 +261,48 @@ public class W4Project {
 		}
 	}
 	
-	//Method for 12
-	public static Boolean willBuyDrink(Boolean isHotOutside, double moneyInPocket) {
-		Boolean buyDrink;
-		if ((isHotOutside == true) && (moneyInPocket > 10.50)) {
-			buyDrink = true;
-		} else {
-			buyDrink = false;
+	//Method for 10
+	public static double findsAverage(double[] arr) {
+		double sum = 0;
+		for (double num : arr) { //for each number in our array, adding it to sum
+			sum += num;
 		}
-		return buyDrink;
+		double average = sum / arr.length; //sum divided by the length of the array to find the average
+		return average;
+	}
+	
+	
+	//Method for 9
+	public static Boolean isGreaterThan100(int[] ages) {
+		Boolean isGreater;
+		int sum = 0; 
+		for (int age : ages) { // for each age in the array, adding it to sum
+			sum += age; 
+		}
+		if (sum > 100) { //if it is greater than 100, will return true
+			isGreater = true;
+		} else {
+			isGreater = false; //if it is less than 100, will return false
+		}
+		return isGreater;
+	}
+	
+	
+	//Method for 8
+	public static String makeFullName(String firstName, String lastName) {
+		String fullName = firstName + " " + lastName; 
+		return fullName;
+	}
+	
+	
+	//Method for 7 
+	public static String wordTimesNum(String word, int n) {
+		StringBuilder newWord = new StringBuilder();
+		for (int i = 0; i < n; i++) { //until loop has gone through n loops, will add the word again
+			newWord.append(word);
+		}
+		
+		return newWord.toString();
 	}
 	
 
